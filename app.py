b06d49e4d3c970e5f21d7f3e2ab369076c7be156
majidkhaja69 <majidkhaja69@gmail.com>
2026-07-05 14:46:17 -0400

    3000

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 sum_result = num1 + num2
-port = 9000
-application = ecommerce
+port = 3000
 print("Sum =", sum_result)
